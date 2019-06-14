295d7dbf692cac10342c6cfca6be769ca2cd0c6d
Christian Strama <wewantthefunk@yahoo.com>
2019-06-14 17:21:16 -0400

    update the action reminder

--- main.go
+++ main.go
@@ -16,29 +16,39 @@ type wordInfo struct {
 	After  []wordInfo `json:"after"`
 }
 
-var listOfWordInfos []wordInfo
+var _listofWordInfos []wordInfo
+
+var _sentence []string
 
 func main() {
 	argsWithoutProg := os.Args[1:]
 
 	action := argsWithoutProg[0]
 
+	rand.Seed(time.Now().UnixNano())
+
 	switch action {
 	case "parse":
 		parseText(readAsText(argsWithoutProg[1]))
-		out, _ := json.Marshal(listOfWordInfos)
+		out, _ := json.Marshal(_listofWordInfos)
 		writeBytes(out, argsWithoutProg[2])
 		break
 	case "import":
-		json.Unmarshal(read(argsWithoutProg[1]), &listOfWordInfos)
-		writeToOutput(listOfWordInfos[0].Word + " - " + strconv.Itoa(listOfWordInfos[0].Count))
+		json.Unmarshal(read(argsWithoutProg[1]), &_listofWordInfos)
+		writeToOutput(_listofWordInfos[0].Word + " - " + strconv.Itoa(_listofWordInfos[0].Count))
 		break
 	case "speech":
-		json.Unmarshal(read(argsWithoutProg[1]), &listOfWordInfos)
-		totalWords := len(listOfWordInfos)
-		rand.Seed(time.Now().UnixNano())
-		pos := rand.Intn(totalWords)
-		startWord := listOfWordInfos[pos]
+		json.Unmarshal(read(argsWithoutProg[1]), &_listofWordInfos)
+		totalWords := len(_listofWordInfos)
+		startWord := wordInfo{}
+		pos := -1
+		if len(argsWithoutProg) > 2 {
+			startWord = getWordPosition(argsWithoutProg[2])
+		} else {
+
+			pos = rand.Intn(totalWords)
+			startWord = _listofWordInfos[pos]
+		}
 		beforeWord := wordInfo{}
 		afterWord := wordInfo{}
 		if len(startWord.Before) > 0 {
@@ -49,15 +59,63 @@ func main() {
 			pos = rand.Intn(len(startWord.After))
 			afterWord = startWord.After[pos]
 		}
-		getWord(startWord, beforeWord, afterWord)
+		getWord(startWord, beforeWord, afterWord, 0, rand.Intn(20-5)+5)
+		writeToOutput(strings.Join(_sentence[:], " "))
 		break
+	case "find":
+		json.Unmarshal(read(argsWithoutProg[1]), &_listofWordInfos)
+		count := 0
+		foundWord := wordInfo{}
+		for _, word := range _listofWordInfos {
+			if word.Word == argsWithoutProg[2] {
+				foundWord = _listofWordInfos[count]
+				break
+			}
+			count++
+		}
+		writeToOutput(foundWord.Word + " - " + strconv.Itoa(foundWord.Count))
+		break
+	default:
+		writeToOutput("no action specified")
 	}
 }
 
-func getWord(startWord wordInfo, beforeWord wordInfo, afterWord wordInfo) {
-	writeToOutput(startWord.Word)
-	writeToOutput(beforeWord.Word)
-	writeToOutput(afterWord.Word)
+func getWord(startWord wordInfo, beforeWord wordInfo, afterWord wordInfo, count int, limit int) {
+	count++
+	if count > limit {
+		return
+	}
+	newStartWord := wordInfo{}
+	newBeforeWord := wordInfo{}
+	newAfterWord := wordInfo{}
+	pos := 0
+	_sentence = append(_sentence, startWord.Word)
+	if afterWord.Word != "" {
+		_sentence = append(_sentence, afterWord.Word)
+	}
+	if len(afterWord.After) > 0 {
+		pos = rand.Intn(len(afterWord.After))
+		newStartWord = afterWord.After[pos]
+	} else {
+		newStartWord = getWordPosition(afterWord.Word)
+		if len(newStartWord.After) > 0 {
+			pos = rand.Intn(len(newStartWord.After))
+			newStartWord = getWordPosition(newStartWord.After[pos].Word)
+		}
+	}
+	if newStartWord.Word == "" {
+		pos = rand.Intn(len(_listofWordInfos))
+		newStartWord = _listofWordInfos[pos]
+	}
+	if len(newStartWord.After) > 0 {
+		pos = rand.Intn(len(newStartWord.After))
+		newAfterWord = newStartWord.After[pos]
+	}
+	if len(newStartWord.Before) > 0 {
+		pos = rand.Intn(len(newStartWord.Before))
+		newBeforeWord = newStartWord.Before[pos]
+	}
+	getWord(newStartWord, newBeforeWord, newAfterWord, count, limit)
 }
 
 func parseText(test string) {
@@ -81,11 +139,11 @@ func parseText(test string) {
 		if count < len(words)-1 {
 			afterWord = words[count+1]
 		}
-		listOfWordInfos = findWord(word, listOfWordInfos, beforeWord, afterWord)
+		_listofWordInfos = findWord(word, _listofWordInfos, beforeWord, afterWord)
 		count++
 	}
 
-	for _, word := range listOfWordInfos {
+	for _, word := range _listofWordInfos {
 		writeToOutput(word.Word + " - " + strconv.Itoa(word.Count))
 	}
 }
@@ -118,4 +176,14 @@ func findWord(word string, wordInfoList []wordInfo, beforeWord string, afterWord
 	}
 
 	return wordInfoList
+}
+
+func getWordPosition(word string) wordInfo {
+	for _, w := range _listofWordInfos {
+		if w.Word == word {
+			return w
+		}
+	}
+
+	return wordInfo{}
 }
